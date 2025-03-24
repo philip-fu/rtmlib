fdee02075e97a1b345ed33e8990c7efb5f261d5f
y0f01wf <philip.fu@walmart.com>
2025-03-24 23:55:18 +0000

    add polygon

--- rtmlib/tools/__init__.py
+++ rtmlib/tools/__init__.py
@@ -73,3 +73,74 @@ def find_susan(
     else:
         print("No circle found.")
         return (None, None, None)
+    
+
+
+#%%
+import cv2
+import numpy as np
+from typing import List
+from shapely.geometry import Polygon
+from shapely import minimum_bounding_circle
+from shapely.affinity import translate, scale
+
+
+
+# %%
+def get_roundness(contour):
+    area = cv2.contourArea(contour)
+    (x, y), radius = cv2.minEnclosingCircle(contour)
+    circle_area = np.pi * radius**2
+    if circle_area == 0:
+        return 0
+    return area / circle_area
+
+
+def find_polygon(
+    image: np.ndarray, # BGR
+    label_point: List[int],
+    lower_green: List[int] = [36, 50, 50],
+    upper_green: List[int] = [86, 255, 255],
+    max_correction_scale: float = 1.2, # relative scale,
+    manual_shift_correction_in_x: int = 0,# in num of pixels,
+    manual_shift_correction_in_y: int = 0, 
+):
+    if isinstance(lower_green, list):
+        lower_green = np.array(lower_green)
+    if isinstance(upper_green, list):
+        upper_green = np.array(upper_green)
+
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    num_labels, labels_im = cv2.connectedComponents(mask)
+    susan_mask = labels_im == labels_im[label_point[0], label_point[1]]
+
+    # plt.imshow(labels_im)
+
+    contours, _ = cv2.findContours(susan_mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+        # Assuming only one object in the mask, take the largest contour
+        largest_contour = max(contours, key=cv2.contourArea)
+        polygon = Polygon(largest_contour[:, 0, :])
+
+        # corrections
+        roundness_of_polygon = polygon.area / minimum_bounding_circle(polygon).area
+        # angle = math.degrees(math.acos(roundness_of_polygon))
+        
+        # scale from btm point
+        points = np.array(polygon.exterior.coords, np.int32)
+        points = points.reshape((-1, 1, 2))
+        bottom_point_of_circle = points[
+            np.argmax(points[:, 0, 1]), 0, :
+        ]
+        correction_scale = (1 - roundness_of_polygon) * (max_correction_scale - 1) + 1
+        print(f"Scaling circle by {correction_scale} originating from bottom.")
+        polygon = scale(polygon, xfact=1, yfact=correction_scale, origin=tuple(bottom_point_of_circle.tolist()))
+
+        print(f"Shifting circle by ({manual_shift_correction_in_x}, {manual_shift_correction_in_y}).")
+        moved_polygon = translate(polygon, xoff=manual_shift_correction_in_x, yoff=-manual_shift_correction_in_y)
+
+        return moved_polygon
+    else:
+        return None
